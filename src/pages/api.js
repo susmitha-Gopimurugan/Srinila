d4b0af64e8e8162cdbc4850d6de5a37d12a445d1
susmitha-Gopimurugan <167347335+susmitha-Gopimurugan@users.noreply.github.com>
2025-10-27 21:25:31 +0530

    updated api

--- src/pages/api.js
+++ src/pages/api.js
@@ -1,28 +1,24 @@
 import axios from 'axios';
 
-const API_URL='http://localhost:7500/';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:7500/';
 
-export const register=async(username,password)=>{
-     
-    try{
-        await axios.post(`${API_URL}register`,{username,password})
-    }catch(err){
-        console.error('Error during login:', err.response ? err.response.data : err.message);
-        throw new Error('Registration Failed')
- 
-    }
+export const register = async (username, password) => {
+  try {
+    await axios.post(`${API_URL}register`, { username, password });
+  } catch (err) {
+    console.error('Error during registration:', err.response ? err.response.data : err.message);
+    throw new Error('Registration Failed');
+  }
+};
 
-
-}
-
-export const login=async(username,password)=>{
-    try{
-        const response= await axios.post(`${API_URL}login`,{username,password})
-        const token = response.data.token;
-        localStorage.setItem('token', token);
-        return token; 
-    }catch(err){
-        console.error('Error during login:', err.response ? err.response.data : err.message);
-        throw new Error('Login Failed');
-    }
-}
+export const login = async (username, password) => {
+  try {
+    const response = await axios.post(`${API_URL}login`, { username, password });
+    const token = response.data.token;
+    localStorage.setItem('token', token);
+    return token;
+  } catch (err) {
+    console.error('Error during login:', err.response ? err.response.data : err.message);
+    throw new Error('Login Failed');
+  }
+};
